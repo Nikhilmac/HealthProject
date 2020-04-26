5271f4f43aadbb1be879843e5d62fd598e3c8b12
Nikhilmac <nikhil.qa124@@gmail.com>
2020-04-26 19:32:18 +0530

    Code Changes

---
+++ HealthProj/src/test/java/SfaxPack/Demo1.java
@@ -0,0 +1,10 @@
+package SfaxPack;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Lord Shiva Pray for Lord");
+	}
+
+}
---
+++ HealthProj/src/test/java/SfaxPack/Sales.java
@@ -0,0 +1,12 @@
+package SfaxPack;
+
+import org.testng.annotations.Test;
+
+public class Sales {
+
+	@Test
+	public void saless() {
+		System.out.println("Lord Shiva Maven Project");
+	}
+
+}
